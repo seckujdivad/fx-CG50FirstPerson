72477635864b9f61acaf64b9e3c22970aa396d18
seckujdivad <juckes1david@gmail.com>
2021-04-01 19:42:14 +0100

    Added colour for out of range cells

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -5,6 +5,7 @@ color_t GetColour(WorldRegion region)
 	switch (region)
 	{
 	case WorldRegion::Black: return COLOR_BLACK;
+	case WorldRegion::OutOfRange: return COLOR_ORANGE;
 	default: return COLOR_PURPLE;
 	}
 }
