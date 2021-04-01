07c6a3e7bdd8648165001148da9089fdba43910f
seckujdivad <juckes1david@gmail.com>
2021-04-01 17:23:19 +0100

    Fixed and separated constexpr and non-constexpr trig functions

--- src/Maths.h
+++ src/Maths.h
@@ -10,7 +10,7 @@ inline constexpr int fact(int x)
 }
 
 template<typename T>
-inline T pow(T x, int y)
+inline T pow(T x, int y) //y must be > 0
 {
 	for (int i = 1; i < y; i++)
 	{
@@ -20,17 +20,27 @@ inline T pow(T x, int y)
 }
 
 template<typename T>
-inline constexpr T pow_constexpr(T x, int y)
+inline constexpr T pow_constexpr(T x, int y) //y must be >= 0
 {
 	return y == 0 ? 1 : x * pow_constexpr(x, y - 1);
 }
 
-inline constexpr float sin(float x)
+inline constexpr float sin_constexpr(float x)
+{
+	return x - (pow_constexpr(x, 3) / fact(3)) + (pow_constexpr(x, 5) / fact(5));
+}
+
+inline float sin(float x)
 {
 	return x - (pow(x, 3) / fact(3)) + (pow(x, 5) / fact(5));
 }
 
-inline constexpr float cos(float x)
+inline constexpr float cos_constexpr(float x)
+{
+	return x - (pow_constexpr(x, 2) / fact(2)) + (pow_constexpr(x, 4) / fact(4));
+}
+
+inline float cos(float x)
 {
 	return x - (pow(x, 2) / fact(2)) + (pow(x, 4) / fact(4));
 }
