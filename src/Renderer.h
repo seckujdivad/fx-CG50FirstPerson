df081f7b152e37a107a8829c6b9bf34fa481328a
seckujdivad <juckes1david@gmail.com>
2021-04-02 00:46:55 +0100

    Removed default draw

--- src/Renderer.h
+++ src/Renderer.h
@@ -10,14 +10,6 @@ color_t GetColour(WorldRegion region);
 template<unsigned int X, unsigned int Y>
 inline void Render(const World<X, Y>& world, const Player& player)
 {
-	for (int x = 10; x < 50; x++)
-	{
-		for (int y = 10; y < 50; y++)
-		{
-			Bdisp_SetPoint_VRAM(x, y, COLOR_BLACK);
-		}
-	}
-
 	constexpr int BORDER_X = 20;
 	constexpr int BORDER_Y = 20;
 	constexpr int SCREEN_HEIGHT = LCD_HEIGHT_PX - (2 * BORDER_Y);
