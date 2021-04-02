318775544b1161212205d8e11d024300004d5605
seckujdivad <juckes1david@gmail.com>
2021-04-02 01:45:37 +0100

    Wrote world ray intersection

--- src/World.h
+++ src/World.h
@@ -3,6 +3,7 @@
 #include <string.h>
 
 #include "Vector.h"
+#include "Maths.h"
 
 enum class WorldRegion
 {
@@ -40,6 +41,7 @@ inline void GenerateWorld(World<X, Y>& target, const char* generator)
 			}
 		}
 	}
+}
 
 template<unsigned int X, unsigned int Y, typename T>
 inline WorldRegion SampleFromWorld(const World<X, Y>& target, Vector<T, 2> sample)
@@ -57,4 +59,88 @@ inline WorldRegion SampleFromWorld(const World<X, Y>& target, Vector<T, 2> sampl
 		return target[position.GetX()][position.GetY()];
 	}
 }
+
+template<unsigned int X, unsigned int Y>
+inline Vector<float, 2> FindFirstIntersection(const World<X, Y>& world, Vector<float, 2> start_pos, float angle)
+{
+	Vector<float, 2> unit_direction;
+	unit_direction.GetX() = cos(angle);
+	unit_direction.GetY() = sin(angle);
+
+	Vector<float, 2> current_pos = start_pos;
+	WorldRegion current_region = SampleFromWorld(world, current_pos);
+	while (current_region == WorldRegion::Air)
+	{
+		enum class Direction
+		{
+			Dir_X,
+			Dir_Y
+		} direction = Direction::Dir_X;
+		if (unit_direction.GetX() == 0.0f)
+		{
+			direction = Direction::Dir_Y;
+		}
+		else if (unit_direction.GetY() == 0.0f)
+		{
+			direction = Direction::Dir_X;
+		}
+		else
+		{
+			float target_x = ceil(current_pos.GetX());
+			float target_y = ceil(current_pos.GetY());
+
+			float x_lambda_increment = fabs(target_x / unit_direction.GetX());
+			float y_lambda_increment = fabs(target_y / unit_direction.GetY());
+
+			if (x_lambda_increment > y_lambda_increment)
+			{
+				direction = Direction::Dir_Y;
+			}
+			else
+			{
+				direction = Direction::Dir_X;
+			}
+		}
+
+		if (direction == Direction::Dir_X)
+		{
+			float target_x = ceil(current_pos.GetX());
+			if (unit_direction.GetX() > 0.0f)
+			{
+				target_x += 1.0f;
+			}
+			else
+			{
+				target_x -= 1.0f;
+			}
+
+			float lambda = (target_x - start_pos.GetX()) / unit_direction.GetX();
+			float target_y = (lambda * unit_direction.GetY()) + start_pos.GetY();
+
+			current_pos.GetX() = target_x;
+			current_pos.GetY() = target_y;
+		}
+		else
+		{
+			float target_y = ceil(current_pos.GetY());
+			if (unit_direction.GetY() > 0.0f)
+			{
+				target_y += 1.0f;
+			}
+			else
+			{
+				target_y -= 1.0f;
+			}
+
+			float lambda = (target_y - start_pos.GetY()) / unit_direction.GetY();
+			float target_x = (lambda * unit_direction.GetX()) + start_pos.GetX();
+			
+			current_pos.GetX() = target_x;
+			current_pos.GetY() = target_y;
+		}
+
+		current_region = SampleFromWorld(world, current_pos);
+	}
+
+	return current_pos;
 }
