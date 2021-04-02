65f1d4a18a57d022265ef323c6043cc85043f58f
seckujdivad <juckes1david@gmail.com>
2021-04-02 01:46:07 +0100

    Renderer is now called and movement keys work

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,11 @@
 #include "World.h"
 #include "Renderer.h"
 #include "Player.h"
+#include "Maths.h"
 
 int main()
 {
 	Bdisp_AllClr_VRAM();
-	Print_OS("Press EXE to exit", 0, 0);
 
 	World<5, 5> world = {};
 	GenerateWorld(world,
@@ -19,17 +19,41 @@ int main()
 		"OOXOO"
 	);
 
-	Player player = { Vector<float, 2>(2.5f), 0.0f };
-
-	Render(world, player);
+	Player player;
+	player.position = Vector<float, 2>(2.5f);
+	player.rotation = 0.0f;
 
 	while (true)
 	{
+		Render(world, player);
+
 		int key;
 		GetKey(&key);
 
-		if (key == KEY_CTRL_EXE) {
-			break;
+		if (key == KEY_CHAR_4)
+		{
+			player.rotation -= 0.25f;
+			Print_OS("-", 0, 0);
+		}
+		else if (key == KEY_CHAR_6)
+		{
+			player.rotation += 0.25f;
+			Print_OS("+", 0, 0);
+		}
+		else if ((key == KEY_CHAR_8) || (key == KEY_CHAR_2))
+		{
+			Vector<float, 2> increment = 0.0f;
+			increment.GetX() = cos(player.rotation);
+			increment.GetY() = sin(player.rotation);
+
+			increment *= 0.25f;
+
+			if (key == KEY_CHAR_2)
+			{
+				increment *= -1.0f;
+			}
+
+			player.position += increment;
 		}
 	}
 
