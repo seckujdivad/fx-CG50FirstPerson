adda84f457dd459a01ef217067ae76922099cda7
seckujdivad <juckes1david@gmail.com>
2021-04-06 14:35:10 +0100

    Path to calculator is now fetched from a text file

--- scripts/move.bat
+++ scripts/move.bat
@@ -1,1 +1,4 @@
-copy /B fx-CG50FirstPerson.g3a "F:/fx-CG50FirstPerson.g3a"
+SETLOCAL
+
+set /P _CALC_PATH=<scripts/calc_path.txt
+copy /B fx-CG50FirstPerson.g3a "%_CALC_PATH%/fx-CG50FirstPerson.g3a""
