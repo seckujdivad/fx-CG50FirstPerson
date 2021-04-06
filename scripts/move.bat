ef29edc103dcb6dd7971474b2c7db79bf0ccbd42
seckujdivad <juckes1david@gmail.com>
2021-04-06 14:37:47 +0100

    Update move.bat

--- scripts/move.bat
+++ scripts/move.bat
@@ -1,4 +1,4 @@
 SETLOCAL
 
 set /P _CALC_PATH=<scripts/calc_path.txt
-copy /B fx-CG50FirstPerson.g3a "%_CALC_PATH%/fx-CG50FirstPerson.g3a""
+copy /B fx-CG50FirstPerson.g3a "%_CALC_PATH%/fx-CG50FirstPerson.g3a"
