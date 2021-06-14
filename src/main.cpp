0bf64b8c8a93ebbbcce8da57e016a95929644d5a
seckujdivad <juckes1david@gmail.com>
2021-06-14 20:56:09 +0100

    Holding a button now repeatedly carries out that action

--- src/main.cpp
+++ src/main.cpp
@@ -23,8 +23,11 @@ int main()
 	player.position = Vector<float, 2>(2.5f);
 	player.rotation = 0.0f;
 
+	bool use_getkey = true;
+
 	while (true)
 	{
+		player.rotation = fmod(player.rotation, PI * 2.0f);
 		Render(world, player);
 
 		{
@@ -42,18 +45,82 @@ int main()
 
 		Bdisp_PutDisp_DD();
 
-		int key;
-		GetKey(&key);
+		enum class ChosenOption
+		{
+			None,
+			GoToGetKey,
+			RotateLeft,
+			RotateRight,
+			MoveForward,
+			MoveBack,
+			ResetPosition
+		} chosen_option = ChosenOption::None;
 
-		if (key == KEY_CHAR_4)
+		if (use_getkey)
+		{
+			int key;
+			GetKey(&key);
+
+			switch (key)
+			{
+			case KEY_CHAR_4: chosen_option = ChosenOption::RotateLeft; break;
+			case KEY_CHAR_6: chosen_option = ChosenOption::RotateRight; break;
+			case KEY_CHAR_8: chosen_option = ChosenOption::MoveForward; break;
+			case KEY_CHAR_2: chosen_option = ChosenOption::MoveBack; break;
+			case KEY_CHAR_0: chosen_option = ChosenOption::ResetPosition; break;
+			}
+
+			use_getkey = false;
+		}
+		else
+		{
+			int column;
+			int row;
+			unsigned short discard;
+			int event_type = GetKeyWait_OS(&column, &row, KEYWAIT_HALTOFF_TIMEROFF, 0, 0, &discard);
+
+			if (event_type == KEYREP_KEYEVENT)
+			{
+				if (row == 0x04 && column == 0x07) //4
+				{
+					chosen_option = ChosenOption::RotateLeft;
+				}
+				else if (row == 0x04 && column == 0x05) //6
+				{
+					chosen_option = ChosenOption::RotateRight;
+				}
+				else if (row == 0x05 && column == 0x06) //8
+				{
+					chosen_option = ChosenOption::MoveForward;
+				}
+				else if (row == 0x03 && column == 0x06) //2
+				{
+					chosen_option = ChosenOption::MoveBack;
+				}
+				else if (row == 0x02 && column == 0x07) //0
+				{
+					chosen_option = ChosenOption::ResetPosition;
+				}
+				else if (row == 0x09 && column == 0x04) //menu
+				{
+					chosen_option = ChosenOption::GoToGetKey;
+				}
+			}
+			else if (event_type == KEYREP_TIMEREVENT || event_type == KEYREP_NOEVENT)
+			{
+				use_getkey = true;
+			}
+		}
+
+		if (chosen_option == ChosenOption::RotateLeft)
 		{
 			player.rotation += 0.25f;
 		}
-		else if (key == KEY_CHAR_6)
+		else if (chosen_option == ChosenOption::RotateRight)
 		{
 			player.rotation -= 0.25f;
 		}
-		else if ((key == KEY_CHAR_8) || (key == KEY_CHAR_2))
+		else if ((chosen_option == ChosenOption::MoveForward) || (chosen_option == ChosenOption::MoveBack))
 		{
 			Vector<float, 2> increment = 0.0f;
 			increment.GetX() = cos(player.rotation);
@@ -61,20 +128,22 @@ int main()
 
 			increment *= 0.25f;
 
-			if (key == KEY_CHAR_2)
+			if (chosen_option == ChosenOption::MoveBack)
 			{
 				increment *= -1.0f;
 			}
 
 			player.position += increment;
 		}
-		else if (key == KEY_CHAR_0)
+		else if (chosen_option == ChosenOption::ResetPosition)
 		{
 			player.position = Vector<float, 2>(2.5f);
 			player.rotation = 0.0f;
 		}
-
-		player.rotation = fmod(player.rotation, PI * 2.0f);
+		else if (chosen_option == ChosenOption::GoToGetKey)
+		{
+			use_getkey = true;
+		}
 	}
 
 	return 0;
